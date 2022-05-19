595e40fbf79d6e0f9a1912f320acf07123eb3e3d
thuysddoox <dothuy302000@gmail.com>
2022-05-19 11:09:25 +0700

    update login FB

--- src/network/api/user.js
+++ src/network/api/user.js
@@ -3,7 +3,9 @@ const USER_URL = `/user`;
 export const getUser = async (access_token, username) => {
   return axiosInstance.get(`${USER_URL}/detail`, { headers: { "Authorization": `Bearer ${access_token}` }, params: { username: username } });
 };
-
+export const getUserFB = async (access_token, userId) => {
+  return axiosInstance.get(`${USER_URL}/detail`, { headers: { "Authorization": `Bearer ${access_token}` }, params: { user_fb: userId } });
+};
 const REGISTER_URL = `/user/register`;
 export const register = async (data) => {
   return axiosInstance.post(`${REGISTER_URL}`, data);
